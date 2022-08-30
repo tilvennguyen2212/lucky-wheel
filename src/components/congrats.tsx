cf2ab471f24a0a5be602ddb528ca57169aec4f18
tuanle161099 <tuanle@descartes.network>
2022-08-30 17:48:48 +0700

    -

--- src/components/congrats.tsx
+++ src/components/congrats.tsx
@@ -1,9 +1,9 @@
+import { MintAvatar, MintSymbol } from '@sen-use/app/dist'
 import IonIcon from '@sentre/antd-ionicon'
-import { util } from '@sentre/senhub'
 
-import { Col, Modal, Row, Typography } from 'antd'
+import { Button, Col, Image, Modal, Row, Typography } from 'antd'
 
-// import BG from 'static/images/bg-popup.svg'
+import BG from 'static/images/bg-popup.svg'
 
 type CongratsProps = {
   visible: boolean
@@ -20,17 +20,35 @@ const Congrats = ({ onClose, visible, prize }: CongratsProps) => {
       closeIcon={<IonIcon name="close-outline" />}
       className="congrats"
     >
-      <Row gutter={[24, 24]}>
+      <Image preview={false} src={BG} />
+      <Row gutter={[16, 16]} className="congrats_content">
         <Col span={24}>
-          <Typography.Title style={{ color: 'red' }} level={5}>
-            Congrats: {util.shortenAddress('23423423sdfsdfsdfssdf')}
+          <Typography.Title level={4} className="gradient-text">
+            Congratulations!
           </Typography.Title>
         </Col>
         <Col span={24}>
-          You have received the :
-          <span style={{ color: '#FFBE45', textTransform: 'uppercase' }}>
-            {prize}
-          </span>
+          <Typography.Text>You have received a reward!</Typography.Text>
+        </Col>
+        <Col span={24} />
+        <Col span={24}>
+          <MintAvatar size={96} mintAddress={prize} />
+        </Col>
+        <Col span={24}>
+          <Typography.Title level={3}>
+            1000 <MintSymbol mintAddress={prize} />
+          </Typography.Title>
+        </Col>
+        <Col span={24} />
+        <Col span={24}>
+          <Button size="large" type="primary" block>
+            CLAIM
+          </Button>
+        </Col>
+        <Col span={24}>
+          <Button size="large" block>
+            SPIN MORE
+          </Button>
         </Col>
       </Row>
     </Modal>
