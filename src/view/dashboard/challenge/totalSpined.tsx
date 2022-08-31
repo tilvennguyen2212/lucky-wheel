b929fb30226ae7588335a18c112eed45671b071f
minhcong <83624415+dmcong@users.noreply.github.com>
2022-08-31 16:58:50 +0700

    lottery info

--- src/view/dashboard/challenge/totalSpined.tsx
+++ src/view/dashboard/challenge/totalSpined.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-
 import { Space, Typography } from 'antd'
+
 import Icon from '@ant-design/icons'
+
+import { useLotteryInfo } from 'hooks/useLotteryInfo'
+import configs from 'configs'
 
 import { ReactComponent as Ticket } from 'static/images/icons/ticket-icon.svg'
 
 const TotalSpined = () => {
+  const lotteryInfo = useLotteryInfo(configs.sol.campaignId)
+
   return (
     <Space size={4} className="remaining-ticket space-middle-icon">
       <Typography.Text>Total spins:</Typography.Text>
       <Typography.Title level={5} className="gradient-text">
-        76
+        {lotteryInfo.totalPicked.toNumber()}
       </Typography.Title>
       <Icon style={{ fontSize: 20 }} component={Ticket} />
     </Space>
