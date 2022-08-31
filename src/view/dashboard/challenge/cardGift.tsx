d299c67bb2cff6679788715f1f55c945274f1adc
thiennv <thiennguyen@descartes.network>
2022-08-31 13:07:50 +0700

    _

--- src/view/dashboard/challenge/cardGift.tsx
+++ src/view/dashboard/challenge/cardGift.tsx
@@ -13,13 +13,13 @@ const CardGift = ({
   amount = 0,
   active,
 }: CardGiftProps) => {
-  const circleCln = active ? 'gift-step-icon active' : 'gift-step-icon'
+  const cardGiftCln = active ? 'card-gift active' : 'card-gift'
   const btnType = status === GiftStatus.Pending ? 'default' : 'primary'
 
   return (
-    <div className="card-gift">
+    <div className={cardGiftCln}>
       <Image src={src} preview={false} />
-      <div className={circleCln}>{amount}</div>
+      <div className="gift-step-icon">{amount}</div>
       <div className="card-gift-btn">
         <Button
           type={btnType}
