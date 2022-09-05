aee7b93944cfb47b3791afc7cc2a6dbb7c0c97ce
minhcong <83624415+dmcong@users.noreply.github.com>
2022-09-05 08:15:40 +0700

    Update header.tsx

--- src/view/admin/header.tsx
+++ src/view/admin/header.tsx
@@ -1,47 +1,78 @@
-import { Button, Col, Row, Select, Space } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
+import { useAppRoute, useWalletAddress } from '@sentre/senhub'
 
-import { setCampaign } from 'model/main.controller'
+import { Button, Select } from 'antd'
+import { Descriptions, PageHeader, Tag, Space } from 'antd'
+
 import { useInitializeCampaign } from 'hooks/admin/useIntializeCampaign'
-
 import { useSelectedCampaign } from 'hooks/useSelectedCampaign'
+import { setCampaign } from 'model/main.controller'
 import { AppState } from 'model'
+import configs from 'configs'
 
 const Header = () => {
   const dispatch = useDispatch()
   const campaigns = useSelector((state: AppState) => state.campaigns)
+  const walletAddress = useWalletAddress()
   const selectedCampaign = useSelectedCampaign()
   const { onInitializeCampaign } = useInitializeCampaign()
+  const { to } = useAppRoute(configs.manifest.appId)
 
+  const campaignData = campaigns[selectedCampaign]
+  const ownCampaign = walletAddress === campaignData.authority.toBase58()
   return (
-    <Row justify="end">
-      <Col span={4} />
-      <Col flex="auto">
-        <Row justify="center">
-          <Col>
-            <Select
-              style={{ width: 120 }}
-              onChange={(campaign) => dispatch(setCampaign({ campaign }))}
-              value={selectedCampaign}
-            >
-              {Object.keys(campaigns).map((address) => (
-                <Select.Option key={address} value={address}>
-                  {address}
-                </Select.Option>
-              ))}
-            </Select>
-          </Col>
-        </Row>
-        <Row justify="center">
-          <Col>{selectedCampaign}</Col>
-        </Row>
-      </Col>
-      <Col span={4}>
-        <Space direction="vertical" align="end" style={{ width: '100%' }}>
-          <Button onClick={onInitializeCampaign}>New Campaign</Button>
+    <PageHeader
+      ghost={false}
+      onBack={() => to('/dashboard')}
+      title="Admin"
+      subTitle={
+        <Space>
+          {selectedCampaign}
+          {ownCampaign ? (
+            <Tag color="success">Your Campaign</Tag>
+          ) : (
+            <Tag color="error">Not Have Permission</Tag>
+          )}
         </Space>
-      </Col>
-    </Row>
+      }
+      style={{ backgroundColor: 'rgba(255, 255, 255, 0.1)' }}
+      extra={[
+        <Select
+          style={{ width: 120 }}
+          onChange={(campaign) => dispatch(setCampaign({ campaign }))}
+          value={selectedCampaign}
+          key="3"
+        >
+          {Object.keys(campaigns).map((address) => (
+            <Select.Option key={address} value={address}>
+              {address}
+            </Select.Option>
+          ))}
+        </Select>,
+        <Button key="2">Print Ticket</Button>,
+        <Button key="1" type="primary" onClick={onInitializeCampaign}>
+          New Campaign
+        </Button>,
+      ]}
+    >
+      <Descriptions size="small" column={2}>
+        <Descriptions.Item label="Address">
+          {selectedCampaign}
+        </Descriptions.Item>
+        <Descriptions.Item label="Ticket Token">
+          {selectedCampaign}
+        </Descriptions.Item>
+        <Descriptions.Item label="Total Ticket">
+          {campaignData.totalTicket.toNumber()}
+        </Descriptions.Item>
+        <Descriptions.Item label="Total Picked">
+          {campaignData.totalPicked.toNumber()}
+        </Descriptions.Item>
+        <Descriptions.Item label="Authority">
+          {campaignData.authority.toBase58()}
+        </Descriptions.Item>
+      </Descriptions>
+    </PageHeader>
   )
 }
 
