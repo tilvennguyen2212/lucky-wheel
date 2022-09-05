a9107c6a79da1aa922e7b91ce814ed20453705d1
minhcong <83624415+dmcong@users.noreply.github.com>
2022-09-05 11:46:58 +0700

    Update useSpin.ts

--- src/hooks/actions/useSpin.ts
+++ src/hooks/actions/useSpin.ts
@@ -4,14 +4,12 @@ import { web3 } from '@project-serum/anchor'
 import { useAvailableTickets } from '../lottery/useAvailableTickets'
 import { useGetEstimateReward } from '../lottery/useGetEstimateReward'
 import { useGetTicketPickerData } from '../lottery/useGetTicketPickerData'
-import { useGetLoseTickets } from 'hooks/lottery/useGetLoseTickets'
 
 export type SpinResult = {}
 
 export const useSpin = (campaign: string) => {
   const availableTickets = useAvailableTickets(campaign)
   const getEstimateReward = useGetEstimateReward(campaign)
-  const getLoseTickets = useGetLoseTickets(campaign)
   const getTicketPickerData = useGetTicketPickerData()
 
   const spin = useCallback(async (): Promise<string> => {
@@ -47,24 +45,9 @@ export const useSpin = (campaign: string) => {
       sendAndConfirm: false,
     })
     tx.add(checkPrize)
-    // Close if lose
-    const loseTickets = await getLoseTickets()
-    for (const loseTicketAddr of Object.keys(loseTickets)) {
-      const { tx: txClose } = await window.luckyWheel.closeTicket({
-        ticket: new web3.PublicKey(loseTicketAddr),
-        sendAndConfirm: false,
-      })
-      tx.add(txClose)
-    }
     await window.luckyWheel.provider.sendAndConfirm(tx)
     return ticket
-  }, [
-    availableTickets,
-    campaign,
-    getEstimateReward,
-    getLoseTickets,
-    getTicketPickerData,
-  ])
+  }, [availableTickets, campaign, getEstimateReward, getTicketPickerData])
 
   return spin
 }
