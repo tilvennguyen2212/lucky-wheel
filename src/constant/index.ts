3694501f5af0ada8875feceec5a67de08400ef26
tuanle161099 <tuanle@descartes.network>
2022-09-08 18:56:48 +0700

    update BG

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -6,9 +6,9 @@ export enum Reward {
 }
 
 export const LIST_BG_WHEEL = [
-  'radial-gradient(47.41% 47.41% at 49.98% 50.04%, #8952CC 10.07%, #B67AFF 58.27%, #D2ADFF 85.66%, #D2ADFF 92.66%, #A462F5 100%)',
+  'radial-gradient(47.41% 47.41% at 49.98% 50.04%, #8952CC 10.07%, #B67AFF 58.27%, #D2ADFF 85.66%, #C799FF 92.66%, #AF76F5 100%)',
   'radial-gradient(47.41% 47.41% at 49.98% 50.04%, #52C4CC 10.07%, #7AF6FF 58.27%, #B3FAFF 85.66%, #85F7FF 92.66%, #53DCE6 100%)',
-  'radial-gradient(47.41% 47.41% at 49.98% 50.04%, #C25F3E 10.07%, #EE7C56 58.27%, #FFAB8F 84.11%, #FFA385 92.66%, #E67049 100%)',
+  'radial-gradient(47.41% 47.41% at 49.98% 50.04%, #415FCC 10.07%, #708FFF 58.27%, #A3B7FF 85.66%, #99AFFF 92.66%, #6987F0 100%)',
 ]
 
 export const SPECIAL_BG =
