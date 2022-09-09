5a122fcb7ad1fd6b9a6780f7e1febc46e2605c58
tuphan-dn <tuphan@descartes.network>
2022-09-09 11:39:44 +0700

    bugfix

--- src/components/wheel/index.tsx
+++ src/components/wheel/index.tsx
@@ -119,7 +119,9 @@ const Wheel = ({ rewards }: WheelProps) => {
         wheel.style.transform = 'rotate(' + deg + 'deg)'
       }, 2000)
     } catch (error) {
-      notifyError(error)
+      notifyError(
+        `The Solana network is experiencing degraded performance, and this leads to your ticket not instantly available. Let's try again in 2-5 minutes later. Details: ${error}`,
+      )
     } finally {
       setTimeout(() => {
         audio.pause()
