747e4dfb127a5d5370fc3a6f405654b4068b18f7
minhcong <83624415+dmcong@users.noreply.github.com>
2022-09-16 11:11:27 +0700

    update ticket

--- src/watcher/tickets.watcher.tsx
+++ src/watcher/tickets.watcher.tsx
@@ -1,27 +1,35 @@
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 import { useDispatch } from 'react-redux'
 import { web3 } from '@project-serum/anchor'
 
 import Watcher from './watcher'
 
 import { initTickets, upsetTicket } from 'model/tickets.controller'
+import { useWalletAddress } from '@sentre/senhub'
 
 // TODO: Config
 const NAME = 'ticket'
-const FILTER: web3.GetProgramAccountsFilter[] = []
 
 const TicketsWatcher = () => {
   const dispatch = useDispatch()
+  const walletAddress = useWalletAddress()
 
   // TODO: init all account data
-  const init = useCallback((data) => dispatch(initTickets(data)), [dispatch])
+  const init = useCallback(
+    async (data) => dispatch(initTickets(data)),
+    [dispatch],
+  )
   // TODO: upset account data
   const upset = useCallback(
     (key: string, value: any) =>
       dispatch(upsetTicket({ address: key, data: value })),
     [dispatch],
   )
+  const FILTER = useMemo((): web3.GetProgramAccountsFilter[] => {
+    return [{ memcmp: { bytes: walletAddress, offset: 40 } }]
+  }, [walletAddress])
 
+  if (!walletAddress) return null
   return (
     <Watcher
       program={window.luckyWheel.program}
